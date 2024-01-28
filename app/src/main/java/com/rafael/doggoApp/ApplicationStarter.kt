d3067b267961aa1982ddfdcd2e8dbd65ec00f874
rafael-cagliari <rafael.cagliari22@gmail.com>
2024-01-28 11:23:37 -0300

    chore: adds application starter to manifest

--- app/src/main/java/com/rafael/doggoApp/ApplicationStarter.kt
+++ app/src/main/java/com/rafael/doggoApp/ApplicationStarter.kt
@@ -1,15 +1,18 @@
-import android.app.Application
+package com.rafael.doggoApp;
+
+import android.app.Application;
 import com.rafael.commons.data.network.commonsDataNetworkModule
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
-class ApplicationStarter: Application(), DIAware {
-    override val di = DI{
-        import(appGeneralModule)
+public class ApplicationStarter :Application(), DIAware {
+
+    private val appGeneralModule = DI.Module(name = "appGeneralModule") {
+        import(commonsDataNetworkModule)
+        import(testModule)
     }
 
-
-    private val appGeneralModule = DI.Module(name = "appGeneralModule"){
-        import(commonsDataNetworkModule)
+    override val di by DI.lazy {
+        import(appGeneralModule)
     }
 }
--- commons/build.gradle.kts
+++ commons/build.gradle.kts
@@ -33,11 +33,11 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     api("org.kodein.di:kodein-di:7.19.0")
+    api("org.kodein.di:kodein-di-framework-android-x:7.19.0")
     api("com.squareup.retrofit2:retrofit:2.9.0")
     api("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation("junit:junit:4.13.2")

aa27b4d1f529143dada932ff53a0996a8b95b0be
rafael-cagliari <rafael.cagliari22@gmail.com>
2024-01-28 11:24:35 -0300

    chore: linting

--- app/src/main/java/com/rafael/doggoApp/ApplicationStarter.kt
+++ app/src/main/java/com/rafael/doggoApp/ApplicationStarter.kt
@@ -1,11 +1,11 @@
-package com.rafael.doggoApp;
+package com.rafael.doggoApp
 
-import android.app.Application;
+import android.app.Application
 import com.rafael.commons.data.network.commonsDataNetworkModule
 import org.kodein.di.DI
 import org.kodein.di.DIAware
 
-public class ApplicationStarter :Application(), DIAware {
+class ApplicationStarter :Application(), DIAware {
 
     private val appGeneralModule = DI.Module(name = "appGeneralModule") {
         import(commonsDataNetworkModule)
